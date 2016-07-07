13e89b8c0bd34f0a1f2ff4fcff2a8b02ea11b457
tenric <minjieni@vip.qq.com>
2016-07-07 14:17:33 +0800

    Initial Commit

---
+++ VersionUpdate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VersionUpdate
+//
+//  Created by 敏杰 倪 on 16/7/7.
+//  Copyright © 2016年 Tenric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
